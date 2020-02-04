428e0ea0e2304ef7dc004f3b130682b2dcbd5d59
DannyGJdeJong <dannygjdejong@gmail.com>
2020-02-04 18:21:36 +0100

    Seperated spell components

--- src/components/SpellCard/SpellCard.js
+++ src/components/SpellCard/SpellCard.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 export default class SpellCard extends Component {
     render () {
-        const { spell: { name, castingTime, components, range, duration, description } } = this.props;
+        const { spell: { name, castingTime, componentV, componentS, componentM, components, range, duration, description } } = this.props;
 
         const Container = styled.div`
         border-radius: 25px;
@@ -11,8 +11,8 @@ export default class SpellCard extends Component {
         padding: 10px;
         display: flex;
         flex-flow: column;
-        width: 500px;
-        min-height: 400px;
+        width: 300px;
+        height: 302px;
         margin: 5px;
         h2, h5, p {
             text-align: center;
@@ -34,7 +34,20 @@ export default class SpellCard extends Component {
 
         const TextBoxLarge = styled(TextBox)`
         flex: 99999;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        height: 100%;
+        p {
+            text-align: left;
+        }
+        `
 
+        const SubText = styled.p`
+        color: #002626;
+        font-size: 12px;
+        float: left;
+        position: relative;
+        font-style: italic;
         p {
             text-align: left;
         }
@@ -51,20 +64,24 @@ export default class SpellCard extends Component {
                         <p>{ castingTime }</p>
                     </TextBox>
                     <TextBox>
-                        <h5>Components</h5>
-                        <p>{ components }</p>
+                        <h5>Range</h5>
+                        <p>{ range }</p>
                     </TextBox>
                 </Row>
                 <Row>
                     <TextBox>
-                        <h5>Range</h5>
-                        <p>{ range }</p>
+                        <h5>Components</h5>
+                        <p>{ [componentV ? 'V' : null, componentS ? 'S' : null, componentM ? 'M' : null].filter((e) => { return e != null }).join(", ") }</p>
                     </TextBox>
                     <TextBox>
                         <h5>Duration</h5>
                         <p>{ duration }</p>
                     </TextBox>
                 </Row>
+                <div>
+                    <SubText>{ components }</SubText>
+                </div>
+
                 <TextBoxLarge>
                     <p>{ description.split(/(?:\r\n|\r|\n)/g).map(s => {return <>{s} <br /></>}) }</p>
                 </TextBoxLarge>
